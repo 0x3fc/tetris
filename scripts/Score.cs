b53ea72954062d7e1b1ad74c32146a991bea8319
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-12-28 21:39:55 -0800

    Display score

--- scripts/Score.cs
+++ scripts/Score.cs
@@ -3,10 +3,17 @@ using System;
 
 public class Score : Control
 {
+    private static Label label;
+
     public static int score = 0;
     public const int BASE_ROW_CLEAR_POINT = 100;
-    public const int SOFT_DROP_SCORE = 2;
-    public const int HARD_DROP_SCORE = 4;
+    public const int SOFT_DROP_SCORE = 1;
+    public const int HARD_DROP_SCORE = 2;
+
+    public override void _Ready()
+    {
+        label = GetNode<Label>("Label");
+    }
 
     public static void AddClearScore(int clearedRows)
     {
@@ -16,10 +23,17 @@ public class Score : Control
         }
 
         score += BASE_ROW_CLEAR_POINT * (int)Math.Pow(2, clearedRows);
+        Rerender();
     }
 
     public static void AddDropScore(bool isSoftDrop, int distance = 1)
     {
         score += isSoftDrop ? SOFT_DROP_SCORE : HARD_DROP_SCORE * distance;
+        Rerender();
+    }
+
+    private static void Rerender()
+    {
+        label.SetText(score.ToString());
     }
 }
